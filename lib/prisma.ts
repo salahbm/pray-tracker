87bb15e04fedfbf9a5cdb2c34e890a9356ef7cd5
im_salah <salahbm@momenti.biz>
2025-04-09 17:50:43 +0900

    fix: vercel deploy

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,3 @@
-import { PrismaPg } from '@prisma/adapter-pg';
-
 import { PrismaClient } from './prisma/client';
 
 const connectionString = process.env.EXPO_SECRET_DATABASE_URL;
@@ -8,23 +6,18 @@ if (!connectionString) {
   throw new Error('EXPO_SECRET_DATABASE_URL is not defined');
 }
 
-// Supabase has connection limits. Keep pool small for serverless.
-const adapter = new PrismaPg({
-  connectionString,
-  max: 3, // keep this low (Supabase free plan allows 10 connections)
-  idleTimeoutMillis: 10000,
-  connectionTimeoutMillis: 2000,
-});
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
 
 let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === 'production') {
-  // Avoid relying on global in Vercel's serverless production
-  prisma = new PrismaClient({ adapter });
+  prisma = new PrismaClient();
 } else {
-  // In development, use a global to avoid re-creating the client on every reload
   if (!global.prisma) {
-    global.prisma = new PrismaClient({ adapter });
+    global.prisma = new PrismaClient();
   }
   prisma = global.prisma;
 }
