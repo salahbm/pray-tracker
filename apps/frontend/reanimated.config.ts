1d5984c7a9504c6c6702a9213d5aa85fe10c2957
salahbm <salahbm.001@gmail.com>
2025-04-14 21:33:16 +0900

    fix: front removed

--- apps/frontend/reanimated.config.ts
+++
@@ -1,9 +0,0 @@
-import {
-  configureReanimatedLogger,
-  ReanimatedLogLevel,
-} from 'react-native-reanimated';
-
-configureReanimatedLogger({
-  level: ReanimatedLogLevel.warn,
-  strict: false, // Disable strict mode
-});
