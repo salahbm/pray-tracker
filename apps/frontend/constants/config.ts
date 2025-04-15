2e5fd244bef7bbfe04b3d2386b954aaae67f7cf9
im_salah <salahbm@momenti.biz>
2025-04-15 17:46:38 +0900

    Save work before subtree add

--- apps/frontend/constants/config.ts
+++
@@ -1,6 +0,0 @@
-import Constants from 'expo-constants';
-
-const ORIGIN = Constants.expoConfig?.extra?.baseUrl;
-const NODE_ENV = Constants.expoConfig?.extra?.NODE_ENV;
-
-export { ORIGIN, NODE_ENV };
